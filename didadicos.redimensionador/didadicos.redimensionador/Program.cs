7f18ace9631612f1150713c3df580137e6d17649
Claudio Nascimento <claudio.alves20112011@hotmail.com>
2023-05-16 09:26:00 -0300

    Threading

--- didadicos.redimensionador/didadicos.redimensionador/Program.cs
+++ didadicos.redimensionador/didadicos.redimensionador/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading;
+
+Console.WriteLine("Iniciando nosso projeto");
+
+Thread thrad = new System.Threading.Thread(Redimensionar);
+thrad.Start();  
+static void Redimensionar()
+{
+    for (int i = 0; i < 10; i++)
+    {
+        Console.WriteLine(i);
+
+        Thread.Sleep(new TimeSpan(0, 0, 3));
+    }
+}
