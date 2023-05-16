fe7b9fd06b4cb6d5c0da895d6e48f17908c5377f
Claudio Nascimento <claudio.alves20112011@hotmail.com>
2023-05-16 10:21:07 -0300

    enumerate files(ler quantidade de arquivos) e filestream (ler arquivos)

--- didadicos.redimensionador/didadicos.redimensionador/Program.cs
+++ didadicos.redimensionador/didadicos.redimensionador/Program.cs
@@ -11,7 +11,7 @@ Console.Read();
 
 static void Redimensionar()
 {
-
+    #region "Diretorios"
     string diretorio_entrada = "Arquivos_Entrada";
     string diretorio_finalizados = "Arquivos_Finalizados";
     string diretorio_redimensionados = "Arquivos_Redimensionados";
@@ -30,10 +30,24 @@ static void Redimensionar()
     {
         Directory.CreateDirectory(diretorio_redimensionados);
     }
-
+    #endregion
 
     while (true)
     {
+        //Meu programa vai olhar para a pasta de entrada
+        //SE tiver arquivo ele irá redimensionar
+        var arquivos_entrada = Directory.EnumerateFiles(diretorio_entrada);
+
+        foreach (var item in arquivos_entrada )
+        {
+            Console.WriteLine(item);
+        };
+
+        //Ler o tamanho que irá redimensionar
+        //Redimensiona
+        //Copia os arquivos redimensionados para a pasta de redimensionados
+        //Move o arquivo de entrada para a pasta de finalizados
+
         Thread.Sleep(new TimeSpan(0, 0, 3));
     }
 }
