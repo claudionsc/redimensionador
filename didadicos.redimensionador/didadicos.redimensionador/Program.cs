f40c80659a46253fc6ce46c8dcb65e57ac6d85f8
Claudio Nascimento <claudio.alves20112011@hotmail.com>
2023-05-16 09:52:39 -0300

    Função que cria pastas (no debug. region

--- didadicos.redimensionador/didadicos.redimensionador/Program.cs
+++ didadicos.redimensionador/didadicos.redimensionador/Program.cs
@@ -3,14 +3,37 @@ using System.Threading;
 
 Console.WriteLine("Iniciando nosso projeto");
 
-Thread thrad = new System.Threading.Thread(Redimensionar);
+Thread thrad = new Thread(Redimensionar);
 thrad.Start();  
+
+Console.Read();
+
+
 static void Redimensionar()
 {
-    for (int i = 0; i < 10; i++)
-    {
-        Console.WriteLine(i);
 
+    string diretorio_entrada = "Arquivos_Entrada";
+    string diretorio_finalizados = "Arquivos_Finalizados";
+    string diretorio_redimensionados = "Arquivos_Redimensionados";
+
+    if (!Directory.Exists(diretorio_entrada))
+    {
+        Directory.CreateDirectory(diretorio_entrada);
+    }
+
+      if (!Directory.Exists(diretorio_finalizados))
+    {
+        Directory.CreateDirectory(diretorio_finalizados);
+    }
+
+      if (!Directory.Exists(diretorio_redimensionados))
+    {
+        Directory.CreateDirectory(diretorio_redimensionados);
+    }
+
+
+    while (true)
+    {
         Thread.Sleep(new TimeSpan(0, 0, 3));
     }
 }
